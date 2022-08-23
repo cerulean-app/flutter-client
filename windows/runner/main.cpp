032ede26fe6fe2745412302e6526356322961d6b
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2022-08-24 03:18:59 +0530

    windows+linux: another application resize

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(350, 550);
+  Win32Window::Size size(720, 540);
   if (!window.CreateAndShow(L"Cerulean Desktop", origin, size)) {
     return EXIT_FAILURE;
   }
