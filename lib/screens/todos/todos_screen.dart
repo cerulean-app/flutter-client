67702456548f7b6c645d7d496b0f3c0f9ae16e87
tierrif <tierriff@gmail.com>
2022-10-06 13:52:29 +0100

    app: add change password dialog

--- lib/screens/todos/todos_screen.dart
+++ lib/screens/todos/todos_screen.dart
@@ -9,6 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
+import '../../utils.dart';
+
 class TodosScreen extends StatefulWidget {
   const TodosScreen({Key? key, required this.debug}) : super(key: key);
 
@@ -20,12 +22,17 @@ class TodosScreen extends StatefulWidget {
 
 class _TodosScreenState extends State<TodosScreen> {
   List<Todo> todos = [];
+  late Timer timer;
+
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController oldPasswordController = TextEditingController();
+  TextEditingController newPasswordController = TextEditingController();
 
   _fetchTodos() {
     final fileStorage = Provider.of<FileStorage>(context, listen: false);
     setState(() => todos = fileStorage.todos);
 
-    Timer.periodic(const Duration(seconds: 5), (timer) {
+    timer = Timer.periodic(const Duration(seconds: 5), (timer) {
       http.get(Uri.parse('$serverUrl/todos'), headers: {
         'authorization': fileStorage.token,
       }).then((response) {
@@ -53,6 +60,7 @@ class _TodosScreenState extends State<TodosScreen> {
     fetchLogout(fileStorage.token).then((response) {
       if (response.statusCode == 200 || response.statusCode == 401) {
         fileStorage.token = '';
+        timer.cancel();
         Navigator.of(context).pushNamed('/');
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
@@ -62,6 +70,118 @@ class _TodosScreenState extends State<TodosScreen> {
         );
       }
     });
+  }
+
+  Future<http.Response> fetchChangePassword(
+      String token, String currentPassword, String newPassword) {
+    const jsonEncoder = JsonEncoder();
+    return http.post(Uri.parse('$serverUrl/changepassword'),
+        headers: {
+          'authorization': token,
+        },
+        body: jsonEncoder.convert({
+          'currentPassword': currentPassword,
+          'newPassword': newPassword,
+        }));
+  }
+
+  void handleChangePassword() {
+    final fileStorage = Provider.of<FileStorage>(context, listen: false);
+    fetchChangePassword(fileStorage.token, oldPasswordController.text,
+            newPasswordController.text)
+        .then((response) {
+      if (response.statusCode == 200) {
+        Navigator.of(context).pop();
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Password changed successfully!',
+                  style: TextStyle(color: Colors.green))),
+        );
+      } else if (response.statusCode == 401 || response.statusCode == 400) {
+        // Handling for 400 too as an old password
+        // not passing through validation is incorrect anyway.
+        // TODO: Bug: snackbar shows behind the dialog.
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Old password does not match!',
+                  style: TextStyle(color: Colors.red))),
+        );
+      }
+    });
+  }
+
+  Future<void> _changePasswordDialog(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Change Password'),
+          content: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Padding(padding: EdgeInsets.only(top: 16.0)),
+                TextFormField(
+                  controller: oldPasswordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Old Password',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your old password!';
+                    }
+
+                    return null;
+                  },
+                ),
+                const Padding(padding: EdgeInsets.only(top: 16.0)),
+                TextFormField(
+                  controller: newPasswordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'New Password',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your new password!';
+                    } else if (!passwordRegExp.hasMatch(value)) {
+                      return invalidPasswordError;
+                    }
+                    return null;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Save'),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  handleChangePassword();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -78,19 +198,27 @@ class _TodosScreenState extends State<TodosScreen> {
               alignment: Alignment.topLeft,
               child: Column(
                 children: <Widget>[
-                  TextButton.icon(
-                    onPressed: handleLogout,
-                    icon: const Icon(Icons.exit_to_app),
-                    label: const Text('Logout'),
-                  ),
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: TextButton.icon(
+                        onPressed: handleLogout,
+                        icon: const Icon(Icons.exit_to_app),
+                        label: const Text('Logout'),
+                      )),
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: TextButton.icon(
+                        onPressed: () => _changePasswordDialog(context),
+                        icon: const Icon(Icons.password),
+                        label: const Text('Change Password'),
+                      )),
                 ],
               ),
             ),
             Align(
-              // TODO: Actual UI with each to-do.
-              alignment: Alignment.center,
-              child: Text(todos.map((todo) => todo.name).join('\n'))
-            ),
+                // TODO: Actual UI with each to-do.
+                alignment: Alignment.center,
+                child: Text(todos.map((todo) => todo.name).join('\n'))),
           ],
         ),
       ),
